33104874d61d43cdddf2feaaeecd7e2920838e1d
Giasin <94991581+Giasinit@users.noreply.github.com>
2025-08-27 08:34:03 +0200

    Update method to set alternate app icon

--- ios/ExpoAlternateAppIconsModule.swift
+++ ios/ExpoAlternateAppIconsModule.swift
@@ -23,7 +23,7 @@ public class ExpoAlternateAppIconsModule: Module {
     private func setAlternateAppIcon(icon: String?, promise: Promise) -> Void {
         Task { @MainActor in
             do {
-                try await UIApplication.shared.setAlternateIconName(icon);
+                try await UIApplication.shared._setAlternateIconName(icon);
 
                 promise.resolve(icon);
             } catch {
