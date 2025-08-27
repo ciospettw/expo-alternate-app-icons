6d9dea945aa40c6d1553b5dad6e6210fbb8e9d53
Giasin <94991581+Giasinit@users.noreply.github.com>
2025-08-27 09:28:26 +0200

    Refactor ExpoAlternateAppIconsModule for clarity

--- ios/ExpoAlternateAppIconsModule.swift
+++ ios/ExpoAlternateAppIconsModule.swift
@@ -1,34 +1,53 @@
 import ExpoModulesCore
+import UIKit
 
 public class ExpoAlternateAppIconsModule: Module {
-    private var supportsAlternateIcons = UIApplication.shared.supportsAlternateIcons;
+  private var supportsAlternateIcons: Bool {
+    return UIApplication.shared.supportsAlternateIcons
+  }
 
-    public func definition() -> ModuleDefinition {
-        Name("ExpoAlternateAppIcons")
+  public func definition() -> ModuleDefinition {
+    Name("ExpoAlternateAppIcons")
 
-        Constants({
-          return [
-            "supportsAlternateIcons": self.supportsAlternateIcons
-          ]
-        })
+    Constants({
+      return [
+        "supportsAlternateIcons": self.supportsAlternateIcons
+      ]
+    })
 
-        AsyncFunction("setAlternateAppIcon", setAlternateAppIcon)
-        Function("getAppIconName", getAppIconName)
+    AsyncFunction("setAlternateAppIconWithoutAlert") { (icon: String?, promise: Promise) in
+      self.setAppIconWithoutAlert(icon, promise: promise)
     }
 
-    private func getAppIconName() -> String? {
-        return UIApplication.shared.alternateIconName;
+    Function("getAppIconName") { () -> String? in
+      return UIApplication.shared.alternateIconName
     }
+  }
 
-    private func setAlternateAppIcon(icon: String?, promise: Promise) -> Void {
-        Task { @MainActor in
-            do {
-                try await UIApplication.shared._setAlternateIconName(icon);
+  private func setAppIconWithoutAlert(_ iconName: String?, promise: Promise) {
+    Task { @MainActor in
+      guard UIApplication.shared.responds(to: #selector(getter: UIApplication.supportsAlternateIcons)),
+            UIApplication.shared.supportsAlternateIcons else {
+        promise.reject(Exceptions.MissingCapability("Alternate icons are not supported on this device."))
+        return
+      }
 
-                promise.resolve(icon);
-            } catch {
-                promise.reject(error);
-            }
+      typealias SetAlternateIconName = @convention(c) (NSObject, Selector, NSString?, @escaping (NSError?) -> Void) -> Void
+      let selectorString = "_setAlternateIconName:completionHandler:"
+      let selector = NSSelectorFromString(selectorString)
+
+      if let method = UIApplication.shared.method(for: selector) {
+        let impl = unsafeBitCast(method, to: SetAlternateIconName.self)
+        impl(UIApplication.shared, selector, iconName as NSString?) { error in
+          if let error = error {
+            promise.reject(error)
+          } else {
+            promise.resolve(iconName)
+          }
         }
+      } else {
+        promise.reject(Exceptions.MissingCapability("Unable to find private selector \(selectorString)"))
+      }
     }
+  }
 }
